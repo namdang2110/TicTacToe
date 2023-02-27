2117a9cb5461d22e794fd7f832ebbbf518f7ac5f
danghoangnam2110 <hodangtennam2110@gmail.com>
2023-02-27 16:13:23 +0700

    Đã có thể chơi 2 người trên cùng 1 thiết bị

---
+++ app/src/main/java/com/example/tictactoe/AddPlayers.kt
@@ -0,0 +1,11 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlayers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_players)
+    }
+}
